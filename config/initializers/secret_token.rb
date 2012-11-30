dd10bef07532122530bc44cd72d92a2ac08e9fa9
YoshifumiSato <yoshifumi0521@gmail.com>
2012-11-30 18:04:01 +0900

    Railsアプリを作成した。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TreeStructureOfCategory::Application.config.secret_token = '3d2722837c012bc49b4e1753b3c885c760ecfb65db1ab74bf3ac918215074e4858fd80d882285bf110e8ae9482915ed6cdfbc6bfe68b35ab4694bfa3fd750c84'
