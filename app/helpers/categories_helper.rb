6bcc35bbab116f0f00624af2443791f29f940303
YoshifumiSato <yoshifumi0521@gmail.com>
2012-12-05 22:20:14 +0900

    子カテゴリーの追加の途中まで実装。

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,19 @@
+#coding: utf-8
+#カテゴリーのヘルパー
 module CategoriesHelper
+
+  #optionの配列をかえす。
+  def option(parents)
+    
+    options = []
+    parents.each do |parent|
+      options << [parent.name ,parent.id] 
+    end
+    return options
+
+  end
+
+
+
+
 end
