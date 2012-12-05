488fa5e16e611692ca952f4bb8b362f280e5f420
YoshifumiSato <yoshifumi0521@gmail.com>
2012-12-05 21:23:12 +0900

    親カテゴリーの追加の部分まで終わった

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,23 +6,24 @@ class CategoriesController < ApplicationController
   
     @categories = Category.all
     @parents = @categories[0].children
-    logger.debug(@parents.inspect)
 
   end
 
+  #カテゴリーや項目をプラスするメソッド
   def new
-
+    
     #パラメーターを値を取り出す。
     type = params[:type]
     if type == "parent"
+       
       #親カテゴリーの追加だったする処理。
       @parent = Category.new
       @parent.parent_id = 1
-
-      return
+    
     elsif type == "children"
       #子カテゴリーの追加の処理。
-
+      @children = Category.new 
+      @parents = Category.where(:parent_id => 1)
 
 
     else
@@ -35,7 +36,30 @@ class CategoriesController < ApplicationController
   end
 
   def create
-  
+
+    type = params["type"]
+    if type == "parent"
+      
+      #@parentにハッシュをいれる。
+      @parent = Category.new()
+      @parent.parent_id = 1
+      @parent.name = params[:category][:name]
+
+
+      if @parent.save
+        #categoriesのindexに戻る。
+        redirect_to :categories
+      else
+        #newアクションに戻る
+        render "new" 
+      end
+
+    elsif type == "children"
+
+
+
+
+    end
   
   
   end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,8 @@
 class Category < ActiveRecord::Base
   attr_accessible :name, :parent_id
 
+  validates :name, presence: true
+
   #ツリー構造にするための記述
   has_many :children,:class_name => "Category",:foreign_key => :parent_id 
   belongs_to :parent,:class_name => "Category",:foreign_key => :parent_id
